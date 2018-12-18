4467f71a0416dc4b5a5e1e9b5c8dda515dde7ac4
julien.hagestedt <julien.hagestedt@gmail.com>
2018-12-18 10:36:37 +0100

    correct profile

--- run-vcloud.sh
+++ run-vcloud.sh
@@ -1,2 +1,2 @@
 mvn clean package
-java -jar -Dspring.profiles.active=azure app/target/peer-registry-app-0.0.1-SNAPSHOT.jar
+java -jar -Dspring.profiles.active=vcloud app/target/peer-registry-app-0.0.1-SNAPSHOT.jar
